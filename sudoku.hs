30846550e972a24270a08022efdd317b5d1cdf82
rkoeninger <rkoeninger@att.net>
2015-02-22 16:55:15 -0500

    Simplified existing code

--- sudoku.hs
+++ sudoku.hs
@@ -1,104 +1,84 @@
-{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ScopedTypeVariables, TupleSections #-}
 
-import Data.Set (Set, difference, fromList, findMin, notMember, singleton, size)
-import qualified Data.Set (filter)
+import Data.Set (Set, difference, fromList, singleton, size, unions, toList)
 import Data.List (nub)
+import Data.Maybe (catMaybes, listToMaybe, fromJust)
 
-type Cell = Set Int
+singleMaybe :: Set a -> Maybe a
+singleMaybe s
+	| isDecided s = listToMaybe (toList s)
+	| otherwise = Nothing
+
+next3 n = [n .. (n + 2)]
+div3 n = (div n 3) * 3
+mod3 n = (mod n 3) * 3
+
+data Value = A | B | C | D | E | F | G | H | I deriving (Eq, Ord, Show, Read)
+listValues = [A, B, C, D, E, F, G, H, I]
+allValues = fromList listValues
+numValues = size allValues
+
+type Cell = Set Value
+isDecided = (1 ==) . size
+
+decidedValues :: [Cell] -> Set Value
+decidedValues cells = fromList (catMaybes (map singleMaybe cells))
+
+allIndicies = [0 .. (numValues - 1)]
+gridCoords = map (zipWith ($) (map (flip (,)) allIndicies) . replicate 9) allIndicies
+relatedCoords p@(r, c) = nub $ filter (/= p) (row ++ col ++ box)
+	where row = map (r,) allIndicies
+	      col = map (,c) allIndicies
+	      box = [(x, y) | x <- next3 (div3 r), y <- next3 (div3 c)]
+
 type Grid = [[Cell]]
-type Coord = (Int, Int)
-
-allPossibles = fromList [1..9]
-allIndicies = [0..8]
-gridCoords = map (\i -> map (\j -> (i, j)) allIndicies) allIndicies
-allCoords = [(r, c) | r <- allIndicies, c <- allIndicies]
-allRowCoords = gridCoords
-allColCoords = map (\j -> map (\i -> (i, j)) allIndicies) allIndicies
-getBoxCoords index = (slice div gridCoords) >>= (slice mod)
-    where slice f list = take 3 (drop ((f index 3) * 3) list)
-allBoxCoords = map getBoxCoords allIndicies
-allGroups = concat [allRowCoords, allColCoords, allBoxCoords]
-blankGrid = replicate 9 (replicate 9 allPossibles)
-setupGrid :: [[Int]] -> Grid = map (map (\x -> if x == 0 then allPossibles else singleton x))
-
-skipOver :: [a] -> Int -> [a]
-skipOver list index = (take index list) ++ (drop (index + 1) list)
-
-setNth :: Int -> a -> [a] -> [a]
-setNth index value list = (take index list) ++ [value] ++ (drop (index + 1) list)
-
-setCoord :: Coord -> a -> [[a]] -> [[a]]
-setCoord (r, c) value matrix = setNth r (setNth c value (matrix !! r)) matrix
-
-updateNth :: Int -> (a -> a) -> [a] -> [a]
-updateNth index f list = setNth index (f $ list !! index) list
-
-updateCoord :: Coord -> (a -> a) -> [[a]] -> [[a]]
-updateCoord coord f matrix = setCoord coord (f $ getAt matrix coord) matrix
-
-hasDup :: (Eq a) => [a] -> Bool
-hasDup xs = length xs /= (length . nub) xs
-
-isDecided :: Cell -> Bool = (== 1) . size
-isExhausted :: Cell -> Bool = (== 0) . size
-isSolved :: Grid -> Bool = (all isDecided) . getAll
-
-getAll :: [[a]] -> [a]
-getAll = concat
-
-getAt :: [[a]] -> Coord -> a
+zipWithCoords = zipWith zip gridCoords
 getAt grid (r, c) = (grid !! r) !! c
 
-getRow :: [[a]] -> Int -> [a]
-getRow = (!!)
+reduceCell :: [Cell] -> Cell -> Cell
+reduceCell otherCells cell = cell `difference` (decidedValues otherCells)
 
-getCol :: [[a]] -> Int -> [a]
-getCol grid index = map (!! index) grid
+deduceCell :: [Cell] -> Cell -> Cell
+deduceCell otherCells cell = if isDecided exclusiveValues then exclusiveValues else cell
+	where exclusiveValues = cell `difference` (unions otherCells)
 
-getBox :: [[a]] -> Int -> [a]
-getBox grid index = (slice div grid) >>= (slice mod)
-    where slice f list = take 3 (drop ((f index 3) * 3) list)
+opGrid :: ([Cell] -> Cell -> Cell) -> Grid -> Grid
+opGrid f grid = (map . map) opCell (zipWithCoords grid)
+	where opCell (rc, cell) = f (map (getAt grid) (relatedCoords rc)) cell
 
-transformCoord :: (Int -> Int -> Int) -> Coord -> Int
-transformCoord f (r, c) = (f r 3) * 3 + (f c 3)
+reduceGrid = opGrid reduceCell
+deduceGrid = opGrid deduceCell
 
-getBoxCoord :: [[a]] -> Coord -> [a]
-getBoxCoord grid coord = getBox grid (transformCoord div coord)
+reduceDeduce :: Grid -> Grid
+reduceDeduce grid = result
+	where nextGrid = deduceGrid (reduceGrid grid)
+	      result | grid == nextGrid = grid
+	             | otherwise = reduceDeduce nextGrid
 
-getRowExcept grid (r, c) = (getRow grid r) `skipOver` c
-getColExcept grid (r, c) = (getCol grid c) `skipOver` r
-getBoxExcept grid coord = (getBoxCoord grid coord) `skipOver` (transformCoord mod coord)
+branch :: Grid -> [Grid]
+branch = error "not implemented"
 
-{- returns duplicates -}
-getRelated :: [[a]] -> Coord -> [a]
-getRelated grid coord = (\f -> f grid coord) =<< [getRowExcept, getColExcept, getBoxExcept]
 
-getDecidedList :: [Cell] -> [Int]
-getDecidedList cells = map findMin (Prelude.filter isDecided cells)
 
-getDecidedSet = fromList . getDecidedList
+-- Test Code
 
-checkGrid :: Grid -> Bool
-checkGrid grid = all check [getRow, getCol, getBox]
-    where check f = all (not . hasDup . getDecidedList) (map (f grid) allIndicies)
+asValues :: [[Int]] -> Grid
+asValues vals = (map . map) interpretVal vals
+	where interpretVal x = case x of
+		0 -> allValues
+		_ -> singleton (listValues !! (x - 1))
 
-validate :: Grid -> Bool
-validate grid = (all (not . isExhausted) (getAll grid)) && (checkGrid grid)
+asInts :: Grid -> [[Int]]
+asInts grid = (map . map) interpretVal grid
+	where interpretVal x = case fromJust (singleMaybe x) of
+		A -> 1
+		B -> 2
+		C -> 3
+		D -> 4
+		E -> 5
+		F -> 6
+		G -> 7
+		H -> 8
+		I -> 9
 
-reduceCell :: Grid -> Coord -> Cell
-reduceCell grid coord = (getAt grid coord) `difference` (getDecidedSet (getRelated grid coord))
-
-deduceCell :: Grid -> Coord -> Cell
-deduceCell grid coord = strip (getAt grid coord)
-    where related = getRelated grid coord
-          vals = Data.Set.filter (\val -> all (notMember val) related) (getAt grid coord)
-          strip cell = if 0 == (size vals) then cell else singleton (findMin vals)
-
-reduce :: Grid -> Grid
-reduce grid = (map . map) (reduceCell grid) gridCoords
-
-deduce :: Grid -> Grid
-deduce grid = (map . map) (deduceCell grid) gridCoords
-
-test :: Grid
-test = (deduce . reduce . deduce . reduce . deduce . reduce . deduce . reduce . deduce . reduce . setupGrid) [[0,0,7,1,8,0,0,4,0],[0,0,4,0,0,0,8,0,0],[8,0,0,0,0,6,2,5,0],[0,4,0,0,9,8,7,1,0],[1,0,0,3,0,5,0,0,6],[0,9,5,2,1,0,0,8,0],[0,2,1,7,0,0,0,0,8],[0,0,3,0,0,0,4,0,0],[0,8,0,0,2,9,1,0,0]]
+testGrid0 = asValues [[0,0,7,1,8,0,0,4,0],[0,0,4,0,0,0,8,0,0],[8,0,0,0,0,6,2,5,0],[0,4,0,0,9,8,7,1,0],[1,0,0,3,0,5,0,0,6],[0,9,5,2,1,0,0,8,0],[0,2,1,7,0,0,0,0,8],[0,0,3,0,0,0,4,0,0],[0,8,0,0,2,9,1,0,0]]

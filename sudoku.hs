9d824c8208c6b796e2699de1f663168f9493c81c
rkoeninger <rkoeninger@att.net>
2015-03-03 22:37:23 -0500

    Added findBranchCoords

--- sudoku.hs
+++ sudoku.hs
@@ -1,8 +1,9 @@
 {-# LANGUAGE TupleSections #-}
 
 import Data.Set (Set, fromList, toList, size, singleton, difference, unions)
-import Data.List (nub)
+import Data.List (nub, minimumBy)
 import Data.Maybe (catMaybes, listToMaybe, fromJust)
+import Data.Function (on)
 
 data Value = A | B | C | D | E | F | G | H | I deriving (Eq, Ord, Show, Read)
 listValues = [A, B, C, D, E, F, G, H, I]
@@ -12,13 +13,10 @@ numValues = size allValues
 type Cell = Set Value
 isDecided = (1 ==) . size
 
-count :: (a -> Bool) -> [a] -> Int
-count f xs = length (filter f xs)
-
 singleMaybe :: Set a -> Maybe a
 singleMaybe s
 	| isDecided s = listToMaybe (toList s)
-	| otherwise = Nothing
+	| otherwise   = Nothing
 
 decidedValues :: [Cell] -> Set Value
 decidedValues cells = fromList (catMaybes (map singleMaybe cells))
@@ -85,12 +83,25 @@ isSolved grid = valid && allDecided
 	where valid = isValid grid
 	      allDecided = all isDecided (concat grid)
 
-branchCandidates :: [Cell] -> [Cell]
-branchCandidates cells = filter moreThan1 cells
-	where moreThan1 cell = 1 < (count (== cell) cells)
+compareBranchCells :: Cell -> Cell -> Ordering
+compareBranchCells g1 g2 = if forgetG1 || forgetG2 then defaultG else order
+	where forgetG1 = isDecided g1
+	      forgetG2 = isDecided g2
+	      defaultG = if forgetG1 then GT else LT
+	      order = on compare size g1 g2
+
+compareBranchCoords :: Grid -> (Int, Int) -> (Int, Int) -> Ordering
+compareBranchCoords grid = compareBranchCells `on` (getAt grid)
+
+findBranchCoords :: Grid -> (Int, Int)
+findBranchCoords grid = minimumBy (compareBranchCoords grid) (concat gridCoords)
 
 branch :: Grid -> [Grid]
 branch = error "not implemented"
+
+solveMaybe :: Grid -> Maybe Grid
+solveMaybe grid = if isSolved reducedGrid then Just reducedGrid else Nothing
+	where reducedGrid = reduceDeduce grid
 
 -- Test Code
 
@@ -113,6 +124,9 @@ asInts grid = (map . map) interpretVal grid
 		H -> 8
 		I -> 9
 
+solveMaybeAsInts :: Grid -> Maybe [[Int]]
+solveMaybeAsInts = fmap asInts . solveMaybe
+
 testGrid0 = asValues [[0,0,7,1,8,0,0,4,0],
                       [0,0,4,0,0,0,8,0,0],
                       [8,0,0,0,0,6,2,5,0],
@@ -132,10 +146,6 @@ testGrid1 = asValues [[9,0,0,5,0,0,2,0,3],
                       [0,0,2,0,0,0,6,0,1],
                       [0,0,0,3,2,0,0,0,0],
                       [8,0,1,0,0,7,0,0,5]]
-
-solveMaybe :: Grid -> Maybe [[Int]]
-solveMaybe grid = fmap asInts (if isSolved grid_ then Just grid_ else Nothing)
-	where grid_ = reduceDeduce grid
 
 {-
 
